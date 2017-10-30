bfca9127eb91de485a417fc6673bfeeff2503b2c
Leo van Kampenhout <l.vankampenhout@uu.nl>
2017-10-30 16:23:27 +0100

    enable batch processing

--- interpolate_to_racmo_topo.py
+++ interpolate_to_racmo_topo.py
@@ -8,54 +8,66 @@
 import numpy as np
 import xarray as xr
 
+case = 'b.e20.BHIST.f09_g17.20thC.190_ramp204_reset.002'
+period = '1980-2005'
+filetype="ymonmean_racmo"
+
 topo_vec    = 'topo_racmo.nc'
-var_vec     = 'tsa_racmo.nc'
-varname = 'TSA'
-topo_target = '/glade/p/work/lvank/racmo/racmo23p2_GRN_monthly/elev.nc'
+#var_vec     = 'tsa_racmo.nc'
 
-d1 = xr.open_dataset(topo_vec)
-print(d1.dims)
-d2 = xr.open_dataset(var_vec)
-print(d2.dims)
-d3 = xr.open_dataset(topo_target)
-print(d3.dims)
-d3.coords['time'] = d2.coords['time']
-print(d3.dims)
+#topo_target = '/glade/p/work/lvank/racmo/racmo23p2_GRN_monthly/elev.nc'
+topo_target = '/Users/leo/workspace/data/racmo/racmo23p2/elev.nc'
 
-h = d1['TOPO_COL'].values[0,:,:,:]
-print(h.shape)
+#varlist = 'TSA'.split()
+varlist = 'EFLX_LH_TOT FGR FIRA FIRE FSA FSH FSM FSR QRUNOFF QSNOFRZ QSNOMELT QSOIL RAIN_REPARTITIONED SNOW_REPARTITIONED TG U10'.split()
 
-v = d2[varname].values[:]
-print(v.shape)
-(ntime, nlev, nrlon, nrlat) = v.shape
+for varname in varlist: 
+   var_vec = "gridded3d_%s_%s_%s_%s.nc" % (varname,case,period,filetype)
+   print(var_vec)
 
-elev = d3['Elevation'].values
-
-# mask missing values
-hh=np.ma.masked_greater(h,1e30)
-vv=np.ma.masked_greater(v,1e30)
-
-#print(h[0,0,0])
-
-td = np.zeros((ntime,nrlon,nrlat)) # result
-print('td',td.shape)
-
-import time
-t1 = time.time()
-
-# Note: this loop takes about 70 sec... Swapping the order of loop indices does not help...
-for itime in np.arange(ntime):
-   for ilon in np.arange(nrlon):
-      for ilat in np.arange(nrlat):
-         td[itime, ilon,ilat] = np.interp(elev[ilon,ilat],hh[:,ilon,ilat],vv[itime,:,ilon,ilat])
-
-t2 = time.time()
-print('elapsed [s]',t2-t1)
-
-d4 = xr.Dataset()
-d4[varname] = (('time','rlat','rlon'), td)
-print(d4)
-
-d4.to_netcdf('tsa_downscaled.nc')
-
+   d1 = xr.open_dataset(topo_vec)
+   print(d1.dims)
+   d2 = xr.open_dataset(var_vec)
+   print(d2.dims)
+   d3 = xr.open_dataset(topo_target)
+   print(d3.dims)
+   d3.coords['time'] = d2.coords['time']
+   print(d3.dims)
+   
+   h = d1['TOPO_COL'].values[0,:,:,:]
+   print(h.shape)
+   
+   v = d2[varname].values[:]
+   print(v.shape)
+   (ntime, nlev, nrlon, nrlat) = v.shape
+   
+   elev = d3['Elevation'].values
+   
+   # mask missing values
+   hh=np.ma.masked_greater(h,1e30)
+   vv=np.ma.masked_greater(v,1e30)
+   
+   #print(h[0,0,0])
+   
+   td = np.zeros((ntime,nrlon,nrlat)) # result
+   print('td',td.shape)
+   
+   import time
+   t1 = time.time()
+   
+   # Note: this loop takes about 70 sec... Swapping the order of loop indices does not help...
+   for itime in np.arange(ntime):
+      for ilon in np.arange(nrlon):
+         for ilat in np.arange(nrlat):
+            td[itime, ilon,ilat] = np.interp(elev[ilon,ilat],hh[:,ilon,ilat],vv[itime,:,ilon,ilat])
+   
+   t2 = time.time()
+   print('elapsed [s]',t2-t1)
+   
+   d4 = xr.Dataset()
+   d4[varname] = (('time','rlat','rlon'), td)
+   print(d4)
+   
+   d4.to_netcdf(varname+'_downscaled.nc')
+   
 
