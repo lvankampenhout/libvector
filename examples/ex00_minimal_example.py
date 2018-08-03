056acd33b0ba9352ad90b032f44cdaa5c27e6904
Leo van Kampenhout <l.vankampenhout@uu.nl>
2018-08-03 08:07:59 -0600

    examples

--- examples/ex00_minimal_example.py
+++ examples/ex00_minimal_example.py
@@ -5,8 +5,12 @@
    
    Read single vector file and convert variable "QICE" to 3d gridded variable
 """
+import sys
 
-from libvector.VectorMecVariable import VectorMecVariable
+# import libvector package from local directory tree
+sys.path.insert(0, "../libvector")
+
+from VectorMecVariable import VectorMecVariable
 
 filename='/glade2/scratch2/lvank/archive/f.e20.FHIST.f09_001/lnd/hist/f.e20.FHIST.f09_001.clm2.h2.1983-05.nc'
 
--- examples/ex01_single_var_2d.py
+++ examples/ex01_single_var_2d.py
@@ -6,21 +6,20 @@
    Read single vector file and convert variable "QICE" to 2d gridded variable
    using wrapper script. Store result in NetCDF.
 """
+import sys
 
-GLC_NEC = 10 # maximum number of elevation classes present in input file
-COLUNIT_GLCMEC = 4 # for landunit types and column types, land ice = 7 (older CLM) land ice = 4 (newer CLM)
+# import libvector package from local directory tree
+sys.path.insert(0, "../libvector")
 
-from netCDF4 import Dataset, default_fillvals
-
-from libvector.VectorMecVariable import VectorMecVariable
-from libvector.vector2gridded2d import vector2gridded2d
+from VectorMecVariable import VectorMecVariable
+from vector2gridded2d import vector2gridded2d
 
 filename='/glade2/scratch2/lvank/archive/f.e20.FHIST.f09_001/lnd/hist/f.e20.FHIST.f09_001.clm2.h2.1983-05.nc'
 
 vmv = VectorMecVariable("QICE", filename)
 
 # Set glacier fraction per MEC column using coupler history file
-#vmv.setGlcFracCPL("/glade2/scratch2/lvank/archive/f.e20.FHIST.f09_001/cpl/hist/f.e20.FHIST.f09_001.cpl.hi.1980-01-01-00000.nc")
+#vmv.setGlcFracCouplerFile("/glade2/scratch2/lvank/archive/f.e20.FHIST.f09_001/cpl/hist/f.e20.FHIST.f09_001.cpl.hi.1980-01-01-00000.nc")
 
 # Set glacier fraction per MEC column using surfdata file
 vmv.setGlcFracSurfdat("/glade/p/cesmdata/cseg/inputdata/lnd/clm2/surfdata_map/surfdata_0.9x1.25_16pfts_Irrig_CMIP6_simyr1850_c170824.nc")
